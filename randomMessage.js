35807aa4a2fee7b23d85f89ab7c9a4bec287669e
D Darsey <187077340+ddarsey@users.noreply.github.com>
2025-02-09 13:09:17 -0800

    Fill in flavors, toppings, blessings

--- randomMessage.js
+++ randomMessage.js
@@ -1,3 +1,6 @@
-const flavors = []
-const toppings = []
-const blessings = []
+const flavors = ["strawberry", "chocolate", "pistachio", "mint"];
+const toppings = ["nuts", "boba", "caramel", "fruit"];
+const blessings = ["Have a lovely day!", "It might rain today", "It's a sunny day", "Your ice cream is on the house!"];
+
+console.log("Hellurr");
+
