904ed3a23e2da3370142befad2c12246d76ea89c
D Darsey <187077340+ddarsey@users.noreply.github.com>
2025-02-09 13:50:05 -0800

    Put options inside an object

--- randomMessage.js
+++ randomMessage.js
@@ -1,14 +1,17 @@
-const flavors = ["strawberry", "chocolate", "pistachio", "mint"];
-const toppings = ["nuts", "boba", "caramel", "fruit", "whipped cream"];
-const blessings = ["Have a lovely day!", "It might rain today", "It's a sunny day", "Your ice cream is on the house!", "BTW you should consider using deodorant."];
+const options = {
+    flavors: ["strawberry", "chocolate", "pistachio", "mint"],
+    toppings: ["nuts", "boba", "caramel", "fruit", "whipped cream"],
+    blessings: ["Have a lovely day!", "It might rain today", "It's a sunny day", "Your ice cream is on the house!", "BTW you should consider using deodorant."],
+};
+
 
 const randomizer = () => {
-    ranNum1 = Math.floor(Math.random() * 4); // There's 4 options, so multiply by 4. 
-    ranNum2 = Math.floor(Math.random() * 5);
-    ranNum3 = Math.floor(Math.random() * 5);
-    ranFlavor = flavors[ranNum1];
-    ranTopping = toppings[ranNum2];
-    ranBlessing = blessings[ranNum3];
+    const ranNum1 = Math.floor(Math.random() * 4); // There's 4 options, so multiply by 4. 
+    const ranNum2 = Math.floor(Math.random() * 5);
+    const ranNum3 = Math.floor(Math.random() * 5);
+    const ranFlavor = options.flavors[ranNum1];
+    const ranTopping =options.toppings[ranNum2];
+    const ranBlessing = options.blessings[ranNum3];
     console.log('Your ice cream flavor is: ' + ranFlavor);
     console.log('Your topping is: ' + ranTopping);
     console.log('Your blessing is: ' + ranBlessing);
